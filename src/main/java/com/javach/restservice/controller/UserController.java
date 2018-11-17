06efa9dea71b5582603488e14c4ea6293364a35e
محمد ايمن شراده <mcharrada@gNET.edu.sa>
2018-11-17 13:19:16 +0300

    delete no need import

--- src/main/java/com/javach/restservice/controller/UserController.java
+++ src/main/java/com/javach/restservice/controller/UserController.java
@@ -1,19 +1,14 @@
 package com.javach.restservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javach.restservice.entity.User;
 import com.javach.restservice.repository.UserRepository;
-
-import javassist.tools.web.BadHttpRequest;
 
 @RestController
 @RequestMapping(path = "/users")
